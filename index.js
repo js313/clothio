4902d0061edd3a3e9cb09bc3b0c2684a5b76d917
Jeenit <sharmajeenit2000@gmail.com>
2022-11-19 21:51:19 +0530

    deployment fixes

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const cloudinary = require('cloudinary').v2
 const path = require('path')
 
 // only when ready to deploy
-// app.use(express.static(path.resolve(__dirname, './client/build')))
+if (process.env.NODE_END === "production")
+    app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
